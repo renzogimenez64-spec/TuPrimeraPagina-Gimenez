cfe412887dd062155329c91afac293dec8e982af
Renzo <renzogimenez64@gmail.com>
2025-09-07 23:12:05 -0300

    actualizacion de campos

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from inicio.views import inicio, crear_libro, lista_libros, detalle_libro
+from inicio.views import inicio, crear_libro, lista_libros, detalle_libro, ActualizarLibro
 
 urlpatterns = [
     path('', inicio, name='inicio'),
     path('crear-libro/', crear_libro, name='crear_libro'),
     path('libro/', lista_libros, name='lista_libros'),
     path('libro/<libro_id>/', detalle_libro, name='detalle_libro'),
+    path('libro/<pk>/actualziacion/', ActualizarLibro.as_view(), name='actualizar_libro'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect 
 from inicio.models import Libro
 from inicio.forms import CrearLibro
+from django.views.generic.edit import UpdateView
+from django.urls import reverse_lazy
 
 def inicio(request):
     return render(request, 'inicio/index.html') 
@@ -34,3 +36,13 @@ def detalle_libro(request, libro_id):
     libro = Libro.objects.get(id=libro_id)
 
     return render(request, 'inicio/detalle_libro.html', {'libro': libro})
+
+# def actualizar_libro(request, libro_id)
+
+class ActualizarLibro(UpdateView):
+
+    model = Libro
+    template_name = "inicio/actualizar_libro.html"
+    fields = "__all__"
+    success_url = reverse_lazy('lista_libros')
+     
